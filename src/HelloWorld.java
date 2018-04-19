c828089660ed3d0b10bbbc11edf32987a6b0d144
Pankajanother <pankajanother@gmail.com>
2018-04-19 16:38:29 +0530

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Seleniumguru.com
+ */
+
+public class HelloWorld
+{
+    public static void main(String[] args){
+        System.out.println("Hello world");//Prints Hello world
+    }
+}
