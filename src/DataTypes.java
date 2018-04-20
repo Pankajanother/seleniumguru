9313694b96008d487f8bde78efb99a4ec1f1b00c
Pankajanother <pankajanother@gmail.com>
2018-04-20 15:13:16 +0530

    Commit

---
+++ src/DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes
+{
+    public static void main(String args[])
+    {
+        byte b1 = 21 ;
+        short s1 = 25222;
+        int i1 = 2502;
+        long l1 = 255522222;
+        double d1 = 255.25;
+        float f1 = 250.2f;
+        boolean b2 = true;
+        char c1 = 'A';
+
+        System.out.println("Byte is:" + b1);
+        System.out.println("Short is:" + s1);
+        System.out.println("Long is:" + l1);
+        System.out.println("Int is:" + i1);
+        System.out.println("Double is:" + d1);
+        System.out.println("Float is:" + f1);
+        System.out.println("Boolean value is:" + b2);
+        System.out.println("Char is" + c1);
+
+    }
+}
