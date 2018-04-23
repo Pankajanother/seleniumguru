ef47200f338b715392b40b734d25b874b771b766
Pankajanother <pankajanother@gmail.com>
2018-04-23 17:23:12 +0530

    Commit

---
+++ src/IfElseIfElse.java
@@ -0,0 +1,18 @@
+public class IfElseIfElse
+    {
+        public static void main(String args[])
+        {
+            int x = 15;
+            if (x < 0)
+
+            {
+                System.out.println("Negative Numbers");
+            } else if (x > 0) {
+                System.out.println("Positve Numbers");
+            } else {
+                System.out.println("Zero");
+            }
+        }
+
+    }
+
